d54b672c93b5678c0abf23546a17685a9070f17e
Maximo Zaragoza <galardonado7@gmail.com>
2023-06-08 18:40:57 -0300

    first commit

---
+++ backend/data.js
@@ -0,0 +1,74 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+  users: [
+    {
+      name: 'Maximo',
+      email: 'maximo-kpo6@hotmail.com',
+      password: bcrypt.hashSync('124467'),
+      isAdmin: true,
+    },
+    {
+      name: 'Pepe',
+      email: 'pepe@hotmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      //_id: '1',
+      name: 'Nike Slim Shirt',
+      slug: 'nsike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      // _id: '2',
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      //_id: '3',
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality Pant',
+    },
+    {
+      // _id: '4',
+      name: 'Adidas Fit pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality Product',
+    },
+  ],
+};
+
+export default data;
---
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -0,0 +1,75 @@
+import React, { useContext, useEffect, useState } from 'react';
+import CheckoutSteps from '../Components/CheckoutSteps';
+import { Helmet } from 'react-helmet-async';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Store } from '../store';
+import { useNavigate } from 'react-router-dom';
+
+function PaymentMethodScreen() {
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { shippingAddress, paymentMethod },
+  } = state;
+
+  const [PaymentMethodName, setPaymentMethodName] = useState(
+    paymentMethod || 'Paypal'
+  );
+  const submitHandler = (e) => {
+    e.preventDefault();
+    ctxDispatch({
+      type: 'SAVE_PAYMENT_METHOD',
+      payload: PaymentMethodName,
+    });
+    localStorage.setItem('paymentMethod', PaymentMethodName);
+    navigate('/placeorder');
+  };
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      navigate('/shipping');
+    }
+  }, [shippingAddress, navigate]);
+
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3></CheckoutSteps>
+      <div className="container small-container">
+        <Helmet>
+          <title>Payment Method</title>
+        </Helmet>
+        <h1 className="my-3">Payment Method</h1>
+        <Form onSubmit={submitHandler}>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              id="Paypal"
+              label="Paypal"
+              value="Paypal"
+              checked={PaymentMethodName === 'Paypal'}
+              onChange={(e) => setPaymentMethodName(e.target.value)}
+            />
+          </div>
+        </Form>
+
+        <Form onSubmit={submitHandler}>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              id="Stripe"
+              label="Stripe"
+              value="Stripe"
+              checked={PaymentMethodName === 'Stripe'}
+              onChange={(e) => setPaymentMethodName(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <Button type="submit">Submit</Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default PaymentMethodScreen;
